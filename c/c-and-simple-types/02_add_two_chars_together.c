f329240a01707857a470f817bfadf34d217a7118
Kaizhao Zhang <zhangkaizhao@gmail.com>
2019-04-15 00:26:33 +0800

    c: Code snippets of C and Simple Types
    via https://blog.jfo.click/c-and-simple-types/

---
+++ c/c-and-simple-types/02_add_two_chars_together.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("%i", '!' + '!'); // -> 66
+    printf("%c", '!' + '!'); // -> B
+    return 0;
+}
---
+++ c/c-and-simple-types/README.txt
@@ -0,0 +1,3 @@
+https://blog.jfo.click/c-and-simple-types/
+
+2019-04-14
